3929e106cc27b43eb24b8d0c41ab89096f1cd311
mprzeklasa <przeklasa.michal96@gmail.com>
2020-01-29 19:42:25 +0100

    zadanie 8 kod

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -2,5 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
+    //ZAD 0
+    var title = document.querySelector('.title');
 
+    function getDataAnimation(element) {
+        var animation = element.dataset.animation;
+        return animation;
+    }
+
+    var result = getDataAnimation(title);
+    console.log(result);
+
+    //ZAD 1
+    var home1 = document.getElementById('home');
+    var home2 = document.querySelector('#home');
+    var data = document.querySelector('data-direction');
+    var block = document.querySelector('.block');
+
+    console.log(home1," ",home2," ",data," ",block," ");
+
+    //ZAD 2
+    var liNav = document.querySelector('nav').querySelectorAll('li');
+    var pDiv = document.querySelectorAll('div p');
+    var divArticle = document.querySelectorAll('article div');
+    console.log(liNav.length, pDiv.length, divArticle.length);
+
+    //ZAD 3
+    var firstArticle = document.querySelector('article.first');
+    var h1 = firstArticle.querySelectorAll('h1');
+    console.log(h1.length);
 });
