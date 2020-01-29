429df71160e5879bf6cfc70b3150cdbbf0ec7f21
mprzeklasa <przeklasa.michal96@gmail.com>
2020-01-29 19:46:11 +0100

    zadanie 9 kod

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -5,8 +5,71 @@ document.addEventListener("DOMContentLoaded", function () {
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
 
-    /*
-    Poniżej napisz kod rozwiązujący zadania
-     */
+    //ZAD 0
 
+    function getDatasInfo(elements)
+    {
+        var array = [];
+        for(var i = 0; i < elements.length; i++) {
+            array.push(elements[i].getAttribute('data-color'));
+        }
+
+        return array;
+    }
+
+    console.log(getDatasInfo(links));
+    //ZAD 1
+    console.log(homeElement);
+    console.log(banner);
+
+    for(let i = 0; i < childElements.length; i++) {console.log(childElements[i].tagName, childElements[i].classList);}
+
+    for(let i = 0; i < blocks.length; i++) {console.log(blocks[i].tagName, blocks[i].classList);}
+
+    for(let i = 0; i < links.length; i++) {console.log(links[i].tagName, links[i].classList);}
+
+    //ZAD 2
+    for(let item of blocks){
+        console.log(item.innerHTML);
+        console.log(item.outerHTML);
+    }
+    blocks.innerHtml = 'Nowa wartość diva o klasie blocks';
+
+    // innerHTML wyświetla strukturę dom wewnątrz elementu
+    // outerHTML wyświetla wraz z elementem
+
+    //ZAD 3
+    var mainFooter = document.querySelector('#mainFooter');
+    console.log(getId(mainFooter));
+
+    function getId(element) {
+        return  element.getAttribute('id');
+    }
+
+    //ZAD 4
+    function getTags(elements){
+        var tags = [];
+        for(let item of elements){
+            tags.push(item.tagName);
+        }
+        return tags;
+    }
+    getTags(childElements);
+
+    //ZAD 5
+    function getClassInfo(element){
+        return element.classList;
+    }
+    getClassInfo(banner);
+
+    //ZAD 6
+    const liElements = document.querySelectorAll('nav li');
+    function setDataDirection(elements){
+        for(let item of elements){
+            if(!item.getAttribute('data-direction')){
+                item.setAttribute('data-direction','top');
+            }
+        }
+    }
+    setDataDirection(liElements);
 });
